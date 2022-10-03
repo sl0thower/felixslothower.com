a7484f7e5b94b377f147542de231e6172d89eb7c
felix_slothower <fe8024sl-s@student.lu.se>
2022-10-03 08:57:24 +0200

    model switching on hover successfully implemented

--- src/sobel.js
+++ src/sobel.js
@@ -1,9 +1,5 @@
 import * as THREE from 'three';
 
-import { GUI } from '../node_modules/three/examples/jsm/libs/lil-gui.module.min.js';
-
-import { OrbitControls } from '../node_modules/three/examples/jsm/controls/OrbitControls.js';
-
 import { EffectComposer } from '../node_modules/three/examples/jsm/postprocessing/EffectComposer.js';
 import { RenderPass } from '../node_modules/three/examples/jsm/postprocessing/RenderPass.js';
 import { ShaderPass } from '../node_modules/three/examples/jsm/postprocessing/ShaderPass.js';
@@ -13,18 +9,23 @@ import { SobelOperatorShader } from '../node_modules/three/examples/jsm/shaders/
 
 import { STLLoader } from '../node_modules/three/examples/jsm/loaders/STLLoader.js';
 
-let camera, scene, renderer, composer;
-
+let camera, scene, renderer, composer, pivot;
+let mesh;
 let effectSobel;
 
-const params = {
-	enable: true
-};
+const targets = {
+	additive:      1,
+	crypto:        2,
+	welding:       3,
+	activity:      4,
+	this_website:  5,
+	about:         6
+}
 
-init();
+init(targets);
 animate();
 
-function init() {
+function init(targets) {
 
 	renderer = new THREE.WebGLRenderer();
     var container = document.getElementById('sobel');
@@ -38,31 +39,35 @@ function init() {
 	scene = new THREE.Scene();
 
 	camera = new THREE.PerspectiveCamera( 70, w/h, 0.1, 200 );
-	camera.position.set( 0, 10, 25 );
+	camera.position.set( 0, 0, 25 );
 	camera.lookAt( scene.position );
 
 	//
 
-	// ASCII file
+	//const loader = new STLLoader();
+	//loader.load( './models/two_plus.stl', function ( geometry ) {
+//
+//		const material = new THREE.MeshPhongMaterial( { color: 0xff5533 } );
+//		const mesh = new THREE.Mesh( geometry, material );
+//
+//		mesh.scale.set(0.5,0.5,0.5)
+//
+//		scene.add( mesh );
+//
+//	} );
 
-	const loader = new STLLoader();
-	loader.load( './models/two_plus.stl', function ( geometry ) {
+	pivot = new THREE.Group();
+	const material = new THREE.MeshPhongMaterial( { color: 0xffff00 } );
 
-		const material = new THREE.MeshPhongMaterial( { color: 0xff5533 } );
+	for ( let k = 0; k <= Object.keys(targets).length; k ++ ) {
+		const geometry = new THREE.TorusKnotGeometry( 7.5, 3, 256, 32, 1, k );
 		const mesh = new THREE.Mesh( geometry, material );
+		mesh.layers.set( k + 1 )
+		mesh.name = Object.keys(targets)[k]
+		pivot.add( mesh )
+	}
 
-		mesh.scale.set(0.5,0.5,0.5)
-
-		scene.add( mesh );
-
-	} );
-
-	//const geometry = new THREE.TorusKnotGeometry( 8, 3, 256, 32, 2, 3 );
-	//const material = new THREE.MeshPhongMaterial( { color: 0xffff00 } );
-
-	//const mesh = new THREE.Mesh( geometry, material );
-	//scene.add( mesh );
-
+	scene.add( pivot )
 	//
 
 	const ambientLight = new THREE.AmbientLight( 0xcccccc, 0.4 );
@@ -93,33 +98,65 @@ function init() {
 	effectSobel.uniforms[ 'resolution' ].value.y = h * window.devicePixelRatio;
 	composer.addPass( effectSobel );
 
-	const controls = new OrbitControls( camera, renderer.domElement );
-	controls.minDistance = 10;
-	controls.maxDistance = 100;
-
 	// Removed resize function since the div should not be changing size
-
 	//window.addEventListener( 'resize', onWindowResize );
 
+	camera.layers.enable(1);
+
+	const buttonAdditive = document.getElementById('additive');
+	buttonAdditive.addEventListener('mouseover', function() {
+
+		switchMesh(targets.additive);
+
+	});
+
+	const buttonCrypto = document.getElementById('crypto');
+	buttonCrypto.addEventListener('mouseover', function() {
+
+		switchMesh(targets.crypto);
+
+	});
+
+	const buttonWelding = document.getElementById('welding');
+	buttonWelding.addEventListener('hover', function() {
+
+		switchMesh(targets.welding);
+
+	});
+
+	const buttonActivity = document.getElementById('activity');
+	buttonActivity.addEventListener('mouseover', function() {
+
+		switchMesh(targets.activity);
+
+	});
+
+	const buttonWebsite = document.getElementById('this_website');
+	buttonWebsite.addEventListener('mouseover', function() {
+
+		switchMesh(targets.this_website);
+
+	});
+
+	const buttonAbout = document.getElementById('about');
+	buttonAbout.addEventListener('mouseover', function() {
+
+		switchMesh(targets.about);
+
+	});
 }
 
-function onWindowResize() {
-
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
-
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	composer.setSize( window.innerWidth, window.innerHeight );
-
-	effectSobel.uniforms[ 'resolution' ].value.x = window.innerWidth * window.devicePixelRatio;
-	effectSobel.uniforms[ 'resolution' ].value.y = window.innerHeight * window.devicePixelRatio;
-
+function switchMesh(target) {
+	camera.layers.disableAll();
+	camera.layers.enable(0);
+	camera.layers.enable(target);
 }
 
 function animate() {
 
+	pivot.rotation.y += 0.005;
+	
+	composer.render();
+
 	requestAnimationFrame( animate );
-
-	composer.render()
-
 }
